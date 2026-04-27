0a64d9318fa7c3fd350e5ed92a3e8d4f7873334c
izumiiu <peerawat.ez@gmail.com>
2026-04-28 04:34:01 +0700

    ExcessPropertiesCheck

--- dist/app.js
+++ dist/app.js
@@ -1,40 +1,13 @@
 "use strict";
+// Excess Properties Check = การตรวจสอบ Peoperties ที่ส่งเข้ามาใน function
 Object.defineProperty(exports, "__esModule", { value: true });
-// Object แบบไม่ได้มีการระบุชนิดข้อมูล----------------------------------
-const person1 = {
-    name: "Peerawat",
-    age: 18
-};
-// Object แบบระบุชนิดข้อมูล------------------------------------------
-const person2 = {
-    name: "Tjay",
-    age: 25
-};
-// const position:{lat:any, long:any} = {lat:30, long:50}
-// function รับค่า object-----------------------------------------------------------------------
 function showDetail(data) {
-    console.log(`Your name is ${data.name} and your age is ${data.age}`);
+    console.log(`ชื่อ = ${data.name} , อายุ = ${data.age}`);
 }
-// แบบ Arrow function**************************
-const showDisplay = (data) => {
-    console.log(`Your name is ${data.name} and your age is ${data.age}`);
-};
-showDetail(person1);
-showDisplay(person2);
-// function ส่งค่า object-------------------------------------------------------------------------
-// const ชื่อฟังชั่น = () : object => { return object }
-const position = { lat: 10, long: 20 };
-const randomAddress = () => {
-    return {
-        address1: Math.random(),
-        address2: Math.random()
-    };
-};
-function randomPosition() {
-    return {
-        lat: Math.random(),
-        long: Math.random()
-    };
-}
-console.log(randomAddress());
+// const person = {
+//     name:"peerawat",
+//     age:30,
+//     address:"กรุงเทพ"
+// }
+showDetail({ name: "peerawat", age: 30, address: "กรุงเทพ" });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,13 @@
+// Excess Properties Check = การตรวจสอบ Peoperties ที่ส่งเข้ามาใน function
 
-// Object แบบไม่ได้มีการระบุชนิดข้อมูล----------------------------------
-const person1 = {
-    name: "Peerawat",
-    age: 18
+function showDetail(data:{name:string, age:number}){
+    console.log(`ชื่อ = ${data.name} , อายุ = ${data.age}`);
 }
 
-// Object แบบระบุชนิดข้อมูล------------------------------------------
-const person2:{name:string, age:number} = {
-    name: "Tjay",
-    age: 25
-}
+// const person = {
+//     name:"peerawat",
+//     age:30,
+//     address:"กรุงเทพ"
+// }
 
-// const position:{lat:any, long:any} = {lat:30, long:50}
-
-// function รับค่า object-----------------------------------------------------------------------
-
-function showDetail(data:{name:string, age:number}):void{
-    console.log(`Your name is ${data.name} and your age is ${data.age}`);
-}
-// แบบ Arrow function**************************
-const showDisplay = (data:{name:string, age:number}):void=>{
-    console.log(`Your name is ${data.name} and your age is ${data.age}`);
-}
-showDetail(person1);
-showDisplay(person2);
-
-// function ส่งค่า object-------------------------------------------------------------------------
-// const ชื่อฟังชั่น = () : object => { return object }
-
-const position:{lat:number, long:number} = {lat:10, long:20}
-
-const randomAddress=():{address1:number, address2:number}=>{
-
-    return{
-        address1: Math.random(),
-        address2: Math.random()
-    }
-}
-
-function randomPosition():{lat:number,long:number}{
-    return{
-        lat: Math.random(),
-        long: Math.random()
-    }
-}
-
-console.log(randomAddress());
-
-
+showDetail({name:"peerawat",age:30,address:"กรุงเทพ"});
