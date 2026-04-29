1dafd2432d61f9afb4d8aa9745df77ea68702c95
izumiiu <peerawat.ez@gmail.com>
2026-04-30 03:13:27 +0700

    Destructuring

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,29 @@
 "use strict";
-// Rest Parameter  = ใช้ในการส่งค่า Parameter(Array) เข้าไปทำงานใน function โดยไม่จำกัดจำนวนโดยใช้เครื่องหมาย ...
+// Destructuring(การสลายโครงสร้าง) = การกำหนดค่าที่อยู่ภายใน Array หรือ Object ไห้กับตัวแปร
+// โดยใช้วิธีการจับคู่ตัวแปรนั้นกับค่าใน Array หรือ Object แทน
+// const colors = ["ขาว", "แดง"]
+// const a = colors[0] // a = ขาว
+// const b = colors[1] // b = แดง
 Object.defineProperty(exports, "__esModule", { value: true });
-// function total (...numbers:number[]){}
-// const total = (...numbers:number[]) => {
-//     const totalSum = numbers.reduce((accumulator, element) => {
-//         return accumulator + element;
-//     }, 0);
-//     return totalSum;
-// }
-const total = (...numbers) => {
-    return numbers.reduce((accumulator, element) => {
-        return accumulator + element;
-    }, 0);
+const colors = ["red", "green", "blue"];
+const [red, green, blue] = colors;
+console.log(red);
+console.log(green);
+console.log(blue);
+// let a:number = 1;
+// let b:number = 2;
+// [a,b] = [b,a];
+// console.log(a);
+// console.log(b);
+// Object Destructuring ------------------------------------------------------------------------------
+const person = {
+    name: "Tjay",
+    age: 18,
+    address: "Phitsanulok"
 };
-console.log(total(100, 200, 300, 400, 500));
+// ส่วนของ Object Destructuring จะใช้เป็น วงเล็บปีกกา {}
+const { name, age, address } = person;
+console.log(name);
+console.log(age);
+console.log(address);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,35 @@
-// Rest Parameter  = ใช้ในการส่งค่า Parameter(Array) เข้าไปทำงานใน function โดยไม่จำกัดจำนวนโดยใช้เครื่องหมาย ...
+// Destructuring(การสลายโครงสร้าง) = การกำหนดค่าที่อยู่ภายใน Array หรือ Object ไห้กับตัวแปร
+// โดยใช้วิธีการจับคู่ตัวแปรนั้นกับค่าใน Array หรือ Object แทน
+// const colors = ["ขาว", "แดง"]
+// const a = colors[0] // a = ขาว
+// const b = colors[1] // b = แดง
 
+const colors:string[] = ["red", "green", "blue"];
+const [red,green,blue] = colors
+console.log(red);
+console.log(green);
+console.log(blue);
 
-// function total (...numbers:number[]){}
+// let a:number = 1;
+// let b:number = 2;
+// [a,b] = [b,a];
+// console.log(a);
+// console.log(b);
 
-// const total = (...numbers:number[]) => {
-//     const totalSum = numbers.reduce((accumulator, element) => {
-//         return accumulator + element;
-//     }, 0);
-//     return totalSum;
-// }
-
-const total = (...numbers:number[]) => {
-    return numbers.reduce((accumulator, element) => {
-        return accumulator + element;
-    }, 0);
+// Object Destructuring ------------------------------------------------------------------------------
+const person = {
+    name: "Tjay",
+    age: 18,
+    address: "Phitsanulok"
 }
-console.log(total(100,200,300,400,500));
+
+// ส่วนของ Object Destructuring จะใช้เป็น วงเล็บปีกกา {}
+const {name, age, address} = person;
+
+console.log(name);
+console.log(age);
+console.log(address);
+
+
+
+
